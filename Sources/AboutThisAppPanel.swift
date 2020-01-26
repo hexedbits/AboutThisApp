90d6d21509ee0a560cb4e4b347929bff7a74e3ec
Jesse Squires <jesse@jessesquires.com>
2020-01-25 21:38:46 -0800

    simplify AboutThisAppPanel

--- Sources/AboutThisAppPanel.swift
+++ Sources/AboutThisAppPanel.swift
@@ -6,25 +6,21 @@
 import Cocoa
 
 public final class AboutThisAppPanel: NSPanel {
-    
+
     public let aboutViewController: AboutThisAppViewController
-    
+
     public init() {
         self.aboutViewController = AboutThisAppViewController()
-        
+
         super.init(contentRect: .zero, styleMask: [.titled, .closable], backing: .buffered, defer: false)
-        
+
         self.contentViewController = self.aboutViewController
-        let viewSize = self.aboutViewController.preferredContentSize
-        self.maxSize = viewSize
-        self.minSize = viewSize
-        
         self.titleVisibility = .hidden
         self.titlebarAppearsTransparent = true
         self.becomesKeyOnlyIfNeeded = false
-        
+
         let screenFrame = NSScreen.main!.frame
-        let leftPoint = NSPoint(x: screenFrame.midX - (viewSize.width / 2), y: screenFrame.maxY)
+        let leftPoint = NSPoint(x: screenFrame.midX, y: screenFrame.maxY)
         self.setFrameTopLeftPoint(leftPoint)
     }
 }
