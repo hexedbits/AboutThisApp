2ca57282d765606139f85bbad898aaf4850a3cd3
Jesse Squires <jesse@jessesquires.com>
2020-01-25 17:40:34 -0800

    add empty example project

---
+++ Example/Sources/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+// Created by Jesse Squires on 1/25/20.
+// Copyright © 2020 Hexed Bits. all rights reserved.
+// 
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Example/Sources/ViewController.swift
@@ -0,0 +1,24 @@
+//
+// Created by Jesse Squires on 1/25/20.
+// Copyright © 2020 Hexed Bits. all rights reserved.
+// 
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
