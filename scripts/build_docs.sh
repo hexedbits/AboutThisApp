ceed377638e46318dd4507d46243e205b40446ab
Jesse Squires <jesse@jessesquires.com>
2020-01-27 16:03:50 -0800

    add lint and docs scripts

---
+++ scripts/build_docs.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#  Created by Jesse Squires
+#  https://www.jessesquires.com
+#
+#  Copyright © 2020-present Jesse Squires
+#
+#  Docs by jazzy
+#  https://github.com/realm/jazzy
+#  ------------------------------
+
+PROJECT="AboutThisApp"
+
+if which jazzy >/dev/null; then
+    jazzy \
+        --clean \
+        --author "Jesse Squires" \
+        --author_url "https://jessesquires.com" \
+        --github_url "https://github.com/hexedbits/$PROJECT" \
+        --module "$PROJECT" \
+        --source-directory . \
+        --readme "README.md" \
+        --documentation "Guides/*.md" \
+        --output docs/
+        exit
+else
+    echo "
+    Error: jazzy not installed! <https://github.com/realm/jazzy>
+    Install: gem install jazzy
+    "
+    exit 1
+fi
